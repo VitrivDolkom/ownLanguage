f9818716867d74ed21c1092a26f336032d1cb0bc
Dmitriy Volkov <vdy0555@gmail.com>
2022-11-19 16:45:08 +0700

    created empty project

---
+++ l.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : l&l.cpp
+// Author      : Dmitriy Volkov
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello l&l" << endl; // prints Hello l&l
+	return 0;
+}
